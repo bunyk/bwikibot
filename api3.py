4c64418d5af768bce708b46ae215c0ca7d89ab23
Bunyk <tbunyk@gmail.com>
2014-10-03 09:11:49 +0300

    updated code to get film data

--- api3.py
+++ api3.py
@@ -1,9 +1,11 @@
 # coding=utf-8
 
-import requests
 import json
 from pprint import pprint
 from itertools import islice
+
+import requests # pip install requests
+import mwparserfromhell # pip install mwparserfromhell
 
 endpoint = 'http://uk.wikipedia.org/w/api.php'
 
@@ -32,21 +34,40 @@ def pages_that_use_template(name):
     ''' Згенерувати послідовність сторінок що використовують шаблон '''
     params = dict(
         action='query',
-        bltitle=name,
-        list='alltransclusions',
-        atlimit='10',
-        atnamespace='0', # брати лише сторінки в основному просторі імен
+        eititle=name,
+        list='embeddedin',
+        eilimit='10',
+        einamespace='0', # брати лише сторінки в основному просторі імен
     )
-    cont = dict(alltransclusions={})
+    cont = dict(embeddedin={})
     while cont:
-        params.update(cont['alltransclusions'])
+        params.update(cont['embeddedin'])
         res = request(**params)
-        for l in res['query']['alltransclusions']:
+        for l in res['query']['embeddedin']:
             yield l['title']
         cont = res.get('query-continue')
 
+def get_film_params(page_text):
+    ''' Отримати словник з параметрами фільму з сторінки '''
+    code = mwparserfromhell.parse(page_text)
+    params = {}
+    for template in code.filter_templates():
+        get_value = lambda name: template.get(name).value.strip()
+        if template.name.matches('Infobox film'):
+            params['uk_title'] = get_value('українська назва')
+            params['title'] = get_value('name')
+            params['director'] = get_value('director')
+            params['budget'] = get_value('budget')
+        if template.name.matches('Фільм'):
+            params['uk_title'] = get_value('українська назва')
+            params['title'] = get_value('оригінальна назва')
+            params['director'] = get_value('режисер')
+            params['budget'] = get_value('бюджет')
+    return params
+
 if __name__ == '__main__':
     for page in islice(pages_that_use_template('Шаблон:Фільм'), 5): 
-        print(repr(page))
-        # print(get_page(page))
+        for pv in get_film_params(get_page(page)).items():
+            print('%s = %s' % pv)
+        print()
 
